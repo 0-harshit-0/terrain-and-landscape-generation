eb7ff56bc1323006c2f5ff6f67d8461d0e52f7d5
harshit singhal <59227793+0-harshit-0@users.noreply.github.com>
2020-11-27 15:41:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://unpkg.com/three@0.123.0/build/three.js';
+import * as THREE from 'https://unpkg.com/three@0.123.0/build/three.module.js';
 import { OrbitControls } from 'https://unpkg.com/three@0.123.0/examples/jsm/controls/OrbitControls.js';
 
 
